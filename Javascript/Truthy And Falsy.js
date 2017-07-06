1a2dfb7f62fee008ec91db67493f50b5853bda5c
Joshua Richard Smith <veryjoshua@gmail.com>
2017-07-06 14:40:29 -0600

    truthy and fasley.js

---
+++ Javascript/Truthy And Falsy.js
@@ -0,0 +1,15 @@
+// In JavaScript there are truthy and falsy values:
+//
+// truthy: a value that when evaluated in a boolean context returns true
+// falsy: a value that when evaluated in a boolean context returns false
+// MDN links:
+//
+// https://developer.mozilla.org/en/docs/Glossary/Truthy
+// https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+// Your task
+//
+// You are given two empty arrays (truthy and falsy) and you have to fill this array with at least 5 elements in each which will evaluate to true or false accordingly.
+
+
+const truthy = ['Hello', 1, 2, 3, 4];
+const falsy = [0, null, NaN, undefined, false];
