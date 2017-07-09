2bf24a8e1051bebf1b10416c0a51b3a5879998bf
Joshua Richard Smith <veryjoshua@gmail.com>
2017-07-09 16:14:48 -0600

    finished the kata find divisors number

---
+++ Javascript/Find Divisors Number.js
@@ -0,0 +1,18 @@
+// Find the number of divisors of a positive integer n.
+//
+// Example:
+//
+// divisors 4  = 3 -- 1, 2, 4
+// divisors 5  = 2 -- 1, 5
+// divisors 12 = 6 -- 1, 2, 3, 4, 6, 12
+// divisors 30 = 8 -- 1, 2, 3, 5, 6, 10, 15, 30
+
+function getDivisorsCnt(n){
+  var numOfDivisorsArr = [];
+  for (var i = 0; i <= n; i++){
+    if(n % i === 0){
+     numOfDivisorsArr.push(i);
+    }
+  }
+ return numOfDivisorsArr.length;
+}
