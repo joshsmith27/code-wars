b6067d8d921b5307177d1811112eb90de5e2c485
Joshua Richard Smith <veryjoshua@gmail.com>
2017-09-02 22:20:16 -0600

    finished kata simple calculator

--- Javascript/is this my tail.js
+++ Javascript/is this my tail.js
@@ -13,3 +13,6 @@ function correctTail(bod, tail){
   }
 }
 // best answer
+function correctTail(bod, tail) {
+  return bod[bod.length-1] === tail;
+}
---
+++ Javascript/simpe calculator.js
@@ -0,0 +1,43 @@
+// You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+//
+// Your function will accept three arguments:
+// The first and second argument will be numbers.
+// The third argument will represent a sign indicating the operation to perform on these two numbers.
+//
+// Example:
+//
+// calculator(1,2,"+"); //=> result will be 3
+// if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+//
+// Example:
+//
+// calculator(1,2,"&"); //=> result will be "unknown value"
+// calculator(1,"k","*"); //=> result will be "unknown value"
+// Good luck!
+
+
+function calculator(a,b,sign){
+  var answer;
+  if(isNaN(a) || isNaN(b)){
+  return "unknown value";
+  }
+
+  switch(sign){
+    case "-":
+      answer = a - b;
+      break;
+    case "+":
+      answer = a + b;
+      break;
+    case "/":
+      answer = a / b;
+      break;
+    case "*":
+      answer = a * b;
+      break;
+    default:
+      answer = "unknown value";
+      break;
+  }
+  return answer;
+}
