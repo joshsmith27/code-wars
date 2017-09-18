bf2510fe969736dc57216880a6ebf4fc3de9cadf
Joshua Richard Smith <veryjoshua@gmail.com>
2017-09-17 22:21:20 -0600

    finished kata difference of volumes of cubiods.js

---
+++ Javascript/difference of volumes of cubiods.js
@@ -0,0 +1,24 @@
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+//
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+//
+// Your function will be tested with pre-made examples as well as random ones.
+//
+// If you can, try writing it in one line of code.
+function findDifference(a, b) {
+  var ar1 = a.reduce(function(sum, value) {
+    return sum * value;
+  }, 1);
+  var ar2 = b.reduce(function(sum, value) {
+    return sum * value;
+  }, 1);
+  if(ar1 < ar2){
+    return ar2 -ar1;
+  }else{
+    return ar1 -ar2;
+  }
+}
+//BEST ANSWER
+function find_difference(a, b) {
+  return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
+}
