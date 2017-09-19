d7cc2e23ca4647603a0d9edb8ae1850f1be80857
Joshua Richard Smith <veryjoshua@gmail.com>
2017-09-18 22:07:46 -0600

    finished kata sum array.cs

---
+++ C#/sum array.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+public class Kata
+{
+  public static double SumArray(double[] array)
+  {
+    return array.Sum();
+  }
+}
+
+// best ANSWER
+using System.Linq;
+
+public class Kata
+{
+  public static double SumArray(double[] array) =>
+    array.Sum();
+}
