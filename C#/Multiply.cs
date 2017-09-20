b7d0ccf734792f33c06f5b616d09f3b514578a87
Joshua Richard Smith <veryjoshua@gmail.com>
2017-09-19 19:20:56 -0600

    finished kata multiply.cs

---
+++ C#/Multiply.cs
@@ -0,0 +1,10 @@
+// The code does not execute properly. Try to figure out why.
+// BUGS
+
+using System.Text;
+using static System.Math;
+public class CustomMath {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
