a65ccd212f2d2d16c5bea5deddffac789f08d329
Joshua Richard Smith <veryjoshua@gmail.com>
2017-10-05 18:12:35 -0600

    finished kata

--- C#/multipy.cs
+++ C#/multipy.cs
@@ -1,4 +1,9 @@
 // This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
-function multiply(a, b){
-  return a * b;
+using System;
+public class Kata
+{
+  public static int Multiply(int a, int b)
+  {
+    return a * b;
+  }
 }
