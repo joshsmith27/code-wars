a553c7e3d5095b84ae3bf11b39a783ac2b06264b
Joshua Richard Smith <veryjoshua@gmail.com>
2017-10-09 10:04:12 -0600

    finished kata

---
+++ C#/oppsite number.cs
@@ -0,0 +1,18 @@
+// Very simple, given a number, find its opposite.
+//
+// Examples:
+//
+// 1: -1
+// 14: -14
+// -34: 34
+// But can you do it in 1 line of code and without any conditionals?
+
+using System;
+
+public class Kata
+    {
+        public  static int Opposite(int number)
+        {
+            return number * -1;
+        }
+    }
