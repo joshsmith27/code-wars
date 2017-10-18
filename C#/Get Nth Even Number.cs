937411d5c45b0d6280502d3884f8e24fdbf5bcff
Joshua Richard Smith <veryjoshua@gmail.com>
2017-10-17 20:53:43 -0600

    finished kata

---
+++ C#/Get Nth Even Number.cs
@@ -0,0 +1,13 @@
+// NthEven(1) //=> 0, the first even number is 0
+// NthEven(3) //=> 4, the 3rd even number is 4 (0, 2, 4)
+//
+// NthEven(100) //=> 198
+// NthEven(1298734) //=> 2597466
+
+public class Kata
+{
+  public static int NthEven(int n)
+  {
+    return n * 2 -2;
+  }
+}
