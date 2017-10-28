dc6f0643e20f952029809065c0c307b75870fe86
Joshua Richard Smith <veryjoshua@gmail.com>
2017-10-28 09:46:47 -0600

    finished kata

---
+++ C#/Convert Number To String.cs
@@ -0,0 +1,18 @@
+// We need a function that can transform a number into a string.
+//
+// What ways of achieving this do you know?
+//
+// Examples:
+//
+// Kata.NumberToString(123) => "123";
+// Kata.NumberToString(999) => "999";
+
+using System;
+
+public class Kata
+{
+  public static string NumberToString(int num)
+  {
+    return num.ToString();
+  }
+}
