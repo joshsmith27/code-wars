0f05da5024fc19ba1467201efd7e4f4db113df6a
Joshua Richard Smith <veryjoshua@gmail.com>
2017-11-09 19:11:47 -0700

    finished kata

---
+++ Javascript/reversed sequence.js
@@ -0,0 +1,11 @@
+// Get the number n to return the reversed sequence from n to 1.
+
+// Example : n==5 >> [5,4,3,2,1]
+
+const reverseSeq = n => {
+    var answer = [];
+    for(var i = n; i > 0; i--){
+      answer.push(i);
+    }
+    return answer;
+  };
