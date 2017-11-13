aeeb76600468e7eff455b435261d9cdc3912f9a0
Joshua Richard Smith <veryjoshua@gmail.com>
2017-11-12 21:32:14 -0700

    finished kata

--- Javascript/16:9.js
+++ Javascript/16:9.js
@@ -2,5 +2,5 @@
 
 // Write a function that accepts arbitrary X and Y resolutions and converts them into resolutions with a 16:9 aspect ratio that maintain equal height. Round your answers up to the nearest integer.
 function aspectRatio(x,y){
-    return [Math.ceil(y*16/9), y];
-    }
+	return [Math.ceil(y*16/9), y];
+}
