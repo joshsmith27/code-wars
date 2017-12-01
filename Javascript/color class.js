a3227c9a7d3c6e257397547fd046026f10632484
Joshua Richard Smith <veryjoshua@gmail.com>
2017-11-30 20:14:32 -0700

    finished kata

---
+++ Javascript/color class.js
@@ -0,0 +1,18 @@
+// Color Ghost
+
+// Create a class Ghost
+
+// Ghost objects are instantiated without any arguments.
+
+// Ghost objects are given a random color attribute of white" or "yellow" or "purple" or "red" when instantiated
+
+// ghost = new Ghost();
+// ghost.color //=> "white" or "yellow" or "purple" or "red"
+
+class Ghost {
+	constructor() {
+		var colors = ['white', 'yellow', 'purple', 'red'];
+		var x = Math.floor((Math.random() * 4));
+		this.color = colors[x];
+	}
+}
