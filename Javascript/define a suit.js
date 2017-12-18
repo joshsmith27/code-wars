17c37bc720122ade37fb138376160a2a756de6ae
Joshua Richard Smith <veryjoshua@gmail.com>
2017-12-17 23:19:03 -0700

    finished kata

---
+++ Javascript/define a suit.js
@@ -0,0 +1,26 @@
+// You get any card as an argument. Your task is to return a suit of this card.
+
+// Our deck (is preloaded):
+
+// deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+//         '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+//         '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+//         '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+    console.log(card);
+    var arr = card.split('');
+    if(arr.includes('♣')){
+      return 'clubs';
+    }else if (arr.includes('♦')){
+      return 'diamonds';
+    }else if (arr.includes('♥')){
+      return 'hearts';
+    }else if (arr.includes('♠')){
+      return 'spades';
+    }    
+  }
