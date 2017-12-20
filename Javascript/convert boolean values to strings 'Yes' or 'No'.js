5255c3073f02b4ec89eb8578683f1eaca66ed533
Joshua Richard Smith <veryjoshua@gmail.com>
2017-12-19 19:43:42 -0700

    finished kata

---
+++ Javascript/convert boolean values to strings 'Yes' or 'No'.js
@@ -0,0 +1,9 @@
+// Complete the bool_to_word (PHP: boolToWord ) method.
+
+// Given: a boolean value
+
+// Return: a 'Yes' string for true and a 'No' string for false
+
+function boolToWord( bool ){
+    return bool ? 'Yes' : 'No';
+  }
