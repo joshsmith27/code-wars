34669754bc52f9f47685ebaeec8a2d192574c05f
Joshua Richard Smith <veryjoshua@gmail.com>
2018-01-08 23:09:28 -0700

    finished kata

---
+++ Javascript/What's wrong with these identifiers.js
@@ -0,0 +1,9 @@
+// An identifier is simply a name...
+// Can you amend this object so that it's properties comprise of vaild identifiers?
+
+var Person = {
+    stname: "John",
+    secondName: "Doe",
+    emailAddress: "john.doe@email.com",
+    malefemale: "M"
+  };
