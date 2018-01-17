122597b1e4d34f9e113ec0f8696aad778a69cec9
Joshua Richard Smith <veryjoshua@gmail.com>
2018-01-16 18:24:09 -0700

    finished kata

---
+++ Javascript/tv guides.js
@@ -0,0 +1,25 @@
+// Program channels into your TV's memory. An array with channels (strings) will be passed as an argument to the function redarr(). Sort the channels in an alphabetical order, remove duplicates and, finally, return an object where each channel (object's value) is assigned to a whole number (objects's key), starting with 0.
+
+// Examples:
+
+// var arr = ["BBC1", "BBC2", "MTV"];
+
+// redarr(arr) // returns {"0":"BBC1", "1":"BBC2", "2":"MTV"}
+
+// var arr = ["BBC1", "BBC1", "BBC2", "MTV"];
+
+// redarr(arr) // returns {"0":"BBC1", "1":"BBC2", "2":"MTV"}
+
+
+
+function redarr(arr) {
+    var answer = {};
+    var sortedArr = arr.sort();
+    var removeDups = sortedArr.filter(function(elem, index, self) {
+         return index == self.indexOf(elem);
+     });
+    for(var i =0; i<removeDups.length; i++){
+     answer[i] = removeDups[i];
+    }  
+   return answer;
+ }
