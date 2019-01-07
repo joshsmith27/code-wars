3d0b93b9b2aa3ceae84ea882490617d64d98f1f1
Joshua Smith <veryjoshua@gmail.com>
2019-01-07 10:22:55 -0500

    Adding Katas

---
+++ C#/Squaring an argument.cs
@@ -0,0 +1,9 @@
+//Now you have to write a function called square that takes an argument and returns the square of it.
+
+using System;
+
+public class Kata {
+    public static double Square (double num) {
+        return Math.Pow (num, 2);
+    }
+}
---
+++ Javascript/JS_W3_Resource/Current_Window.js
@@ -0,0 +1,7 @@
+/// Write a JavaScript program to print (like on a piece of paper) the contents of the current window.
+
+const currentWindow = () => {
+    window.print()
+}
+
+currentWindow()
