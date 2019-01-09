e93f3494b528f9e7b65067a9abc02715de57f7ee
Joshua Smith <veryjoshua@gmail.com>
2019-01-09 09:32:31 -0500

    adding stuff

--- Javascript/JS_W3_Resource/Area of Triangle.js
+++ Javascript/JS_W3_Resource/Area of Triangle.js
@@ -1,3 +1,6 @@
+//Write a JavaScript function to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+
+
 const triangleArea = (a, b, c) => {
     const perimeter = (a + b + c) / 2;
     return Math.sqrt(perimeter * ((perimeter - a) * (perimeter - b) * (perimeter - c)));
