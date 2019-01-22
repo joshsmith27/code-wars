8b3a42769e2480b799831d67370356049f55164e
Joshua Smith <veryjoshua@gmail.com>
2019-01-22 11:22:17 -0500

    Adding a kata

---
+++ C#/Returning Strings.cs
@@ -0,0 +1,14 @@
+// Make a function that will
+// return a greeting statement that uses an input;
+// your program should
+// return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+using System;
+
+public static class Kata {
+    public static string Greet (string name) {
+        return String.Format ("Hello, {0} how are you doing today?", name);
+    }
+}
