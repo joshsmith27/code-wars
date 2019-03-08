794b045bc708b8be8df9790d0f13532439f6ee83
Joshua Smith <veryjoshua@gmail.com>
2019-03-07 20:55:01 -0500

    adding kata

---
+++ C#/Shortest Word.cs
@@ -0,0 +1,12 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+using System.Linq;
+public class Kata
+{
+    public static int FindShort(string s)
+    {
+        return s.Split(" ").OrderBy(c => c.Length).FirstOrDefault().Length;
+    }
+}
